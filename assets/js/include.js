1aca41579fc6967cdaeb7ff1c1fc2ad98648fb20
zizo_leeinjoon <lee@zizo.nejp>
2015-11-26 16:17:25 +0900

    jpgが。。

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,7 +1,6 @@
 (function($){
     var obj      = { path : 'img/', extension : '.png' },
-        filename = obj.path + window.location.href.match(".+/(.+?)\.[a-z]+([\?#;].*)?$")[1] + obj.extension,
-        rand     = Math.floor( Math.random() * 1000 ) ;
+        filename = obj.path + window.location.href.match(".+/(.+?)\.[a-z]+([\?#;].*)?$")[1] + obj.extension;
     $(function(){
         $('img').attr('src', filename);
         jQuery.event.add(window, 'load', function(){
@@ -11,16 +10,19 @@
             function delay1() {
                 var d = new $.Deferred;
                 setTimeout(function(){
-                    var isSrc     = $('img').height() > 100 ? obj.extension = '.png' : obj.extension = '.jpg';
+                    var isSrc = $('img').height() > 100 ? obj.extension = '.png' : obj.extension = '.jpg';
+                    filename  = obj.path + window.location.href.match(".+/(.+?)\.[a-z]+([\?#;].*)?$")[1] + obj.extension;
                     $('img').attr('src', filename);
                     d.resolve();
-                }, 500);
+                }, 1000);
                 return d.promise();
             }
             function delay2() {
-                $('body').height($('img').height()).css({
-                    'background':'url(' + filename + ') center no-repeat'
-                });
+                setTimeout(function(){
+                    $('body').height($('img').height()).css({
+                        'background':'url(' + filename + ') center no-repeat'
+                    });
+                }, 500);
             }
             function delay3() {
                 $('img').hide();
